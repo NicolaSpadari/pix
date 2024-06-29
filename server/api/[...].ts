90a90dc53770e1587e11f3ca0eb4c076b9fc0fba
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-06-29 11:17:24 +0200

    Fix font

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,9 +1,10 @@
 import sharp from "sharp";
 
 const getTitle = (width: number, height: number, text?: string) => {
-	if (text && text !== "") return text;
-	return `${width} x ${height}`
-}
+	if (text && text !== "")
+		return text;
+	return `${width} x ${height}`;
+};
 
 export default defineEventHandler(async (event) => {
 	const size = event.context.params?._ || "500";
@@ -11,21 +12,29 @@ export default defineEventHandler(async (event) => {
 
 	let width, height;
 
-	if(size.includes("x")){
+	if (size.includes("x")) {
 		width = Number(size.split("x")[0]);
 		height = Number(size.split("x")[1]);
-	}else{
+	} else {
 		width = height = Number(size);
 	}
 
 	const svg = `
         <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+			<defs>
+				<style>
+					@import url('https://fonts.googleapis.com/css2?family=Inter');
+					.inter{
+						font-family: Inter;
+					}
+				</style>
+			</defs>
             <rect width="100%" height="100%" fill="#ccc" />
-            <text x="50%" y="53%" font-size="35" font-family="Arial, sans-serif" dominant-baseline="middle" text-anchor="middle" fill="#9c9c9c">
+            <text x="50%" y="53%" font-size="50" class="inter" dominant-baseline="middle" text-anchor="middle" fill="#9c9c9c">
                 ${getTitle(width, height, text?.toString())}
             </text>
         </svg>
     `;
 
-    return sharp(Buffer.from(svg)).webp();
-})
+	return sharp(Buffer.from(svg)).webp();
+});
